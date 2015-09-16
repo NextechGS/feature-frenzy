fc8496e0b45e73ec33a4cfa541d810b0baec6371
Andreas Hocevar <andreas.hocevar@gmail.com>
2015-09-16 14:34:53 +0900

    Optimize the vector-rendering example

--- examples/vector-rendering.js
+++ examples/vector-rendering.js
@@ -1,6 +1,6 @@
 function updateStatitics() {
-  var layer = map.getLayers().item(1);
-  var features = layer.getSource().getFeatures();
+  var layer = map.getLayers().item(0);
+  var features = countries.getFeatures();
   var count = 0;
   for (var i = 0, ii = features.length; i<ii; ++i) {
     count += (features[i].getGeometry().getFlatCoordinates().length / 2);
@@ -13,7 +13,7 @@ function updateStatitics() {
   var simplifiedVertexCount = count;
   var extent = map.getView().calculateExtent(map.getSize());
   extent = ol.extent.extend(extent, layer.getRenderBuffer() * map.getView().getResolution());
-  features = layer.getSource().getFeaturesInExtent(extent);
+  features = countries.getFeaturesInExtent(extent);
   count = 0;
   for (i = 0, ii = features.length; i<ii; ++i) {
     count += (features[i].getGeometry().getSimplifiedGeometry(ol.renderer.vector.getSquaredTolerance(map.getView().getResolution(), ol.has.DEVICE_PIXEL_RATIO)).getFlatCoordinates().length / 2);
@@ -53,20 +53,11 @@ var cityStyles = [new ol.style.Style({
   text: text
 })];
 
-var highlight = new ol.layer.Vector({
-  source: new ol.source.Vector({
-    spatialIndex: false,
-  }),
-  style: new ol.style.Style({
-    fill: new ol.style.Fill({
-      color: '#bada55'
-    })
-  })
-});
-
 var countries = new ol.source.Vector({
   url: 'data/countries.geojson',
-  format: new ol.format.GeoJSON()
+  format: new ol.format.GeoJSON({
+    defaultDataProjection: 'EPSG:3857'
+  })
 });
 
 countries.once('change', function() {
@@ -76,7 +67,6 @@ countries.once('change', function() {
 var map = new ol.Map({
   target: 'map',
   layers: [
-    highlight,
     new ol.layer.Vector({
       name: 'countries',
       source: countries
@@ -88,24 +78,17 @@ var map = new ol.Map({
         format: new ol.format.GeoJSON()
       }),
       style: function(feature, resolution) {
-        text.setText(resolution < 5000 ? feature.get('NAME') : '');
-        return cityStyles;
+        if (resolution < 5000) {
+          text.setText(feature.get('NAME'));
+          return cityStyles;
+        }
       }
     })
   ],
   view: new ol.View({
     center: [0, 0],
-    zoom: 2
+    zoom: 1
   })
-});
-
-map.on('pointermove', function(evt) {
-  highlight.getSource().clear();
-  map.forEachFeatureAtPixel(evt.pixel, function(feature) {
-    highlight.getSource().addFeature(feature);
-  }, this, function(layer) {
-    return (layer.get('name') == 'countries');
-  });
 });
 
 map.on('moveend', updateStatitics);
