0557760af023df524e829356bc8af50250ea9117
Tim Schaub <tim.schaub@gmail.com>
2015-09-17 14:24:02 +0900

    Local projection

--- examples/raster-reprojection.js
+++ examples/raster-reprojection.js
@@ -1,5 +1,8 @@
 proj4.defs('ESRI:54009', '+proj=moll +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 ' +
     '+units=m +no_defs');
+proj4.defs("EPSG:5179","+proj=tmerc +lat_0=38 +lon_0=127.5 +k=0.9996 +x_0=1000000 +y_0=2000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
+
+// ol.proj.get('EPSG:5179').setExtent([531371.84, 967246.47, 1576674.68, 2274021.31]);
 ol.proj.get('ESRI:54009').setExtent([-18e6, -9e6, 18e6, 9e6]);
 
 var map = new ol.Map({
@@ -25,7 +28,7 @@ var map = new ol.Map({
   ],
   view: new ol.View({
     center: [0, 0],
-    zoom: 1
+    zoom: 3
   })
 });
 
@@ -34,6 +37,7 @@ projection.addEventListener('change', function() {
   var view = map.getView();
   var newProj = projection.options[projection.selectedIndex].value;
   var oldProj = view.getProjection();
+
   map.setView(new ol.View({
     projection: newProj,
     center: ol.proj.transform(view.getCenter(), oldProj, newProj),
